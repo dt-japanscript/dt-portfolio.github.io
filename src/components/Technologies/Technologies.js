a8ba2607217a5fee40d49e65a817f82deb777053
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2022-02-26 14:54:57 +0900

    Create content for Technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiPython, DiMootoolsBadge } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id='text'>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      There are many technoligies
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiMootoolsBadge size='3rem' />
+        <ListContainer>
+          <ListTitle>CAD Softwares</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            NX <br />
+            SolidWorks
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiMootoolsBadge size='3rem' />
+        <ListContainer>
+          <ListTitle>CAE Softwares</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            ANSA <br />
+            META
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPython size='3rem' />
+        <ListContainer>
+          <ListTitle>Programming</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Python <br />
+            JavaScript
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
